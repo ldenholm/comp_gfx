b9bd9d94419d6dfed3f114fa6eaacd7bcab21af4
lochie <disrute@gmail.com>
2024-05-29 17:39:13 +1000

    dynamic coloring

--- comp_gfx/Main.cpp
+++ comp_gfx/Main.cpp
@@ -7,15 +7,25 @@ using namespace std;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+	"vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = ourColor;\n"
+"}\n\0";
+
+const char* secondFragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
 "}\n\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int w, int h)
@@ -30,24 +40,32 @@ float vertices[] = {
 	-0.5f,  0.5f, 0.0f   // top left 
 };
 
-float myTestTri[] = {
+float topTriVertices[] = {
 	-0.5f, 0.0f, 0.0f, // centre left  0
 	-0.5f, 0.5f, 0.0f, // top left  1
 	0.0f, 0.5f, 0.0f, // centre top  2
 	0.5f, 0.0f, 0.0f, // centre right  3
 	0.5f, 0.5f, 0.0f // top right 4
-	-0.5f, -0.5f, 0.0f, // bottom left 5
-	0.0f, -0.5f, 0.0f, // centre bottom 6
-	0.5f, -0.5f, 0.0f // bottom right 7
-	-0.25f, 0.25f, 0.0f // half left half top
 };
 
-unsigned int squareTri[] = {
+unsigned int topTris[] = {
 	0, 1, 2, // top left tri
 	2, 4, 3, // top right tri
-	0, 5, 6, // bot left tri
-	3, 7, 6 // bot right tri
 };
+
+float botTriVertices[] = {
+	-0.5f, -0.5f, 0.0f, // bot left 0
+	0.5f, 0.5f, 0.0f, // bot right 1
+	0.0f, 0.5f, 0.0f, // top mid 2
+	0.0f, 0.0f, 0.0f, // origin 3
+	-0.5f, 0.0f, 0.0f // centre left 4
+};
+
+float botTris[] = {
+	0, 1, 2,
+	0, 3, 4
+};
+
 
 unsigned int indices[] = {
 	0, 1, 3, // first tri
@@ -104,9 +122,16 @@ int main() {
 		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << log << endl;
 	}
 
-	// create element buffer obj
-	unsigned int EBO;
-	glGenBuffers(1, &EBO);
+	// compile second frag shader
+	unsigned int secondFragShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(secondFragShader, 1, &secondFragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, log);
+		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << log << endl;
+	}
 	
 	// init shader program
 	unsigned int shaderProgram = glCreateProgram();
@@ -124,6 +149,7 @@ int main() {
 	// clean up resources
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	glDeleteShader(secondFragShader);
 	
 	 /*
 	 copy vertex data to buffer memory
@@ -138,12 +164,30 @@ int main() {
 	// bind vertex array then bind vertex buffers and finally config vertex attributes
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(myTestTri), myTestTri, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(topTriVertices), topTriVertices, GL_STATIC_DRAW);
+
+	// SETUP EBO
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(squareTri), squareTri, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(topTris), topTris, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	
+
+	// CREATE SECOND VBO, VAO, EBO
+	unsigned int VBO_SECOND, VAO_SECOND, EBO_SECOND;
+	glGenVertexArrays(1, &VAO_SECOND);
+	glGenBuffers(1, &VBO_SECOND);
+	glBindVertexArray(VAO_SECOND);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_SECOND);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(botTriVertices), botTriVertices, GL_STATIC_DRAW);
+	// SECOND EBO
+	glGenBuffers(1, &EBO_SECOND);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_SECOND);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(botTris), botTris, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
 
 	// Render loop
 	while (!glfwWindowShouldClose(window))
@@ -156,11 +200,19 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		// draw triangle
+		// dynamic coloring
+		float time = glfwGetTime();
+		float grnVal = (sin(time) / 2.0f) + 0.5f;
+		int vtxClrLoc = glGetUniformLocation(shaderProgram, "ourColor");
 		glUseProgram(shaderProgram);
+		glUniform4f(vtxClrLoc, 0.0f, grnVal, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		// draw multiple tris specified in element buffer
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
+		// RENDER VERTICES STORED IN SECOND PAIR OF (VAO, VBO)
+		glBindVertexArray(VAO_SECOND);
 		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
