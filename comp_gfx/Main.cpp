10a39e4e22c463a8d659248096d08679d4324634
lochie <disrute@gmail.com>
2025-03-29 14:52:46 +1100

    Add structure, revert to original tri, todo fix render

--- comp_gfx/Main.cpp
+++ comp_gfx/Main.cpp
@@ -7,25 +7,20 @@ using namespace std;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-"out vec4 vertexColor;\n"
+//"layout (location = 1) in vec3 aColor;\n"
+//"out vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-	"vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+"   vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	//"ourColor = aColor;\n"
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+//"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = ourColor;\n"
-"}\n\0";
-
-const char* secondFragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+//"   FragColor = ourColor;\n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\n\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int w, int h)
@@ -33,46 +28,35 @@ void framebuffer_size_callback(GLFWwindow* window, int w, int h)
 	glViewport(0, 0, w, h);
 }
 
+//float topTriVertices[] = {
+//	// positions       // colors
+//	-0.5f, 0.0f, 0.0f,  // centre left  0
+//	-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // top left  1
+//	0.0f, 0.5f, 0.0f,  0.0f, 0.0f, 1.0f, // centre top  2
+//	0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, // centre right  3
+//	0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top right 4
+//};
+
+
+// Normalized Device Coordinates (NDC)
+// viewport transforms these to screen-space coords.
 float vertices[] = {
-	 0.5f,  0.5f, 0.0f,  // top right
-	 0.5f, -0.5f, 0.0f,  // bottom right
-	-0.5f, -0.5f, 0.0f,  // bottom left
-	-0.5f,  0.5f, 0.0f   // top left 
+	-0.5f, -0.5f, 0.0f, // left bot
+	 0.5f, -0.5f, 0.0f, // right bot
+	 0.0f,  0.5f, 0.0f // centre top
 };
 
-float topTriVertices[] = {
-	-0.5f, 0.0f, 0.0f, // centre left  0
-	-0.5f, 0.5f, 0.0f, // top left  1
-	0.0f, 0.5f, 0.0f, // centre top  2
-	0.5f, 0.0f, 0.0f, // centre right  3
-	0.5f, 0.5f, 0.0f // top right 4
-};
 
 unsigned int topTris[] = {
 	0, 1, 2, // top left tri
 	2, 4, 3, // top right tri
 };
 
-float botTriVertices[] = {
-	-0.5f, -0.5f, 0.0f, // bot left 0
-	0.5f, 0.5f, 0.0f, // bot right 1
-	0.0f, 0.5f, 0.0f, // top mid 2
-	0.0f, 0.0f, 0.0f, // origin 3
-	-0.5f, 0.0f, 0.0f // centre left 4
-};
-
-float botTris[] = {
-	0, 1, 2,
-	0, 3, 4
-};
-
-
-unsigned int indices[] = {
-	0, 1, 3, // first tri
-	1, 2, 3 // second tri
-};
 
 int main() {
+	// I N I T  A N D  W I N D O W  S E T U P
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -97,11 +81,16 @@ int main() {
 
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 
-	// setup vertex buffer, element buffer, and shader objects
+	// V E R T E X  S H A D E R //
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER); // VAO;
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
+	
 	// check compile successful and capture logs.
 	int success;
 	char log[512];
@@ -111,9 +100,16 @@ int main() {
 		glGetShaderInfoLog(vertexShader, 512, NULL, log);
 		cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << log << endl;
 	}
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
+
+	// F R A G M E N T  S H A D E R //
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
+	
 	// check fragment shader compiled successfully
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if (!success)
@@ -121,23 +117,18 @@ int main() {
 		glGetShaderInfoLog(fragmentShader, 512, NULL, log);
 		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << log << endl;
 	}
-
-	// compile second frag shader
-	unsigned int secondFragShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(secondFragShader, 1, &secondFragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, log);
-		cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << log << endl;
-	}
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 	
-	// init shader program
+	
+	// S H A D E R  P R O G R A M  S E T U P
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 	unsigned int shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
+	
 	// check shader program attached & linked correctly
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success)
@@ -149,47 +140,55 @@ int main() {
 	// clean up resources
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
-	glDeleteShader(secondFragShader);
+
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 	
+	// B U F F E R  O B J E C T S  S E T U P
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
+
 	 /*
 	 copy vertex data to buffer memory
 	 the final argument instructs the gpu how to handle the data.
 	 static draw corresponds to data set once and reused many times.
 	 */
 
-	// setup vertex array and buffer objects
+	// V E R T E X  B U F F E R
+	// -------------------------------------
+	// -------------------------------------
 	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	// bind vertex array then bind vertex buffers and finally config vertex attributes
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(topTriVertices), topTriVertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// -------------------------------------
+	// -------------------------------------
 
-	// SETUP EBO
+	// E L E M E N T  B U F F E R
+	// -------------------------------------
+	// -------------------------------------
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(topTris), topTris, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// -------------------------------------
+	// -------------------------------------
+
+	// D R A W  S O M E T H I N G
+	// -------------------------------------
+	// -------------------------------------
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
 
-	// CREATE SECOND VBO, VAO, EBO
-	unsigned int VBO_SECOND, VAO_SECOND, EBO_SECOND;
-	glGenVertexArrays(1, &VAO_SECOND);
-	glGenBuffers(1, &VBO_SECOND);
-	glBindVertexArray(VAO_SECOND);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO_SECOND);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(botTriVertices), botTriVertices, GL_STATIC_DRAW);
-	// SECOND EBO
-	glGenBuffers(1, &EBO_SECOND);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_SECOND);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(botTris), botTris, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-
-	// Render loop
+	// R E N D E R I N G
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 	while (!glfwWindowShouldClose(window))
 	{
 		// handle input
@@ -212,7 +211,6 @@ int main() {
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		
 		// RENDER VERTICES STORED IN SECOND PAIR OF (VAO, VBO)
-		glBindVertexArray(VAO_SECOND);
 		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
@@ -220,7 +218,14 @@ int main() {
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 
+	// E N D
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
+	// -----------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------
 }
