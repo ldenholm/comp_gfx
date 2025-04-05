74b087838990326892aeb30dbff91bd53a6773a0
lochie <disrute@gmail.com>
2025-04-05 11:29:56 +1100

    TRANSLATION WORKS!!!

--- comp_gfx/VertexIO.vs.glsl
+++ comp_gfx/VertexIO.vs.glsl
@@ -1,8 +1,9 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
+uniform mat4 gTranslation;
 out vec4 vertexColor;
 void main()
 {
-	gl_Position = vec4(aPos, 1.0); 
+	gl_Position = vec4(aPos, 1.0) * gTranslation;
 	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);
 };
