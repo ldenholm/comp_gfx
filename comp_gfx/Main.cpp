94a1d3f3220a145a9327d1c97b3c246069601560
lochie <disrute@gmail.com>
2025-04-08 11:00:58 +1000

    scaffold rotation matrix

--- comp_gfx/Main.cpp
+++ comp_gfx/Main.cpp
@@ -29,6 +29,8 @@ void color_cycle_red(unsigned int& shaderProgram)
 	glUniform4f(vertexColorLoc, (freq + 0.1f), (freq - 0.6f), (freq - 0.9f), 0.4f);
 }
 
+// todo: put transformations into their own .h/.cpp
+
 void translate(unsigned int& shaderProgram)
 {
 	static float scale = 0.0f;
@@ -45,6 +47,49 @@ void translate(unsigned int& shaderProgram)
 	glUseProgram(shaderProgram);
 	glUniformMatrix4fv(gTranslationLocation, 1, GL_FALSE, &translationMatrix[0][0]);
 
+}
+
+void rotate(unsigned int& shaderProgram)
+{
+	static float scale = 0.0f;
+	static float delta = 0.005f;
+	scale += delta;
+	if ((scale >= 0.3f) || (scale <= -0.6f))
+	{
+		delta *= -1.0f;
+	}
+	glm::mat4 rotationMatrix = glm::mat4(1.0f);
+	// this rotation matrix is anchored to the x axis
+	// let R be a rotation matrix, R in R^(4x4) since we account for z axis and w column
+	/*
+	*	note a = current angle, b is desired angle, (x1, y1) = current coords, (x2, y2) = desired coords.
+		
+		use angle sum identity:
+		sin(a + b) = sin(a)cos(b) + cos(a)sin(b)
+		cos(a + b) = cos(a)cos(b) - sin(a)sin(b)
+
+		for unit circle note:
+		sin(a) = opp/hyp = opp/1 = opp = y1
+		cos(a) = adj/hyp = adj/1 = adj = x1
+
+		cos(a + b) = x2
+		sin(a + b) = y2
+
+		putting it all together:
+		x2 = y1*cos(b) + x1*sin(b)
+		y2 = x1*cos(b) - y1*sin(b)
+
+		reorder:
+		x1*cos(b) - y1*sin(b)
+		x1*sin(b) + y1*cos(b)
+
+		recognize Ax = b structure:
+		[cos(b) -sin(b) 0 0]	 [x]
+		|sin(b)  cos(b) 0 0|   x |y|
+		|0          0   1 0|	 |z|
+		[0          0   0 1]	 [w]
+	
+	*/
 }
 
 // Normalized Device Coordinates (NDC)
